36938ab8b403fe44323a07e1ed541ec75c971895
Guilherme Olivera <guilherme.oliveira@viannasempre.com.br>
2025-12-27 18:10:01 -0300

    Teste

--- service/API/Organizacao.ts
+++ service/API/Organizacao.ts
@@ -11,7 +11,7 @@ export interface Response {
 }
 
 const api = axios.create({
-  baseURL: 'https://api.bellory.com.br/api',
+  baseURL: 'https://api-dev.bellory.com.br/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
